7fad565ef9fdcda3d0d086d6576d9ea5451fc71b
vaughanmortgages-design <vaughanmortgages@gmail.com>
2026-08-02 21:21:27 -0400

    Fix homepage LeadScout card to open embedded borrower form

--- assets/estack.js
+++ assets/estack.js
@@ -1,1 +1,7 @@
 document.querySelector('[data-menu]')?.addEventListener('click',e=>{const n=document.querySelector('[data-nav]');const open=n.classList.toggle('open');e.currentTarget.setAttribute('aria-expanded',String(open))});
+
+document.querySelectorAll('a[href="https://leadscout.ca/?ref=ZX3kIzQG"], a[href="https://leadscout.ca/?ref=ZX3klzQG"]').forEach(link=>{
+  link.setAttribute('href','/leadscout-canada-loans.html');
+  link.removeAttribute('target');
+  link.setAttribute('rel','');
+});
